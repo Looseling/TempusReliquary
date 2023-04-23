c5159d45362530a736b31f1dd052a8594c8757d2
Khan <batyrkhan.akzholov@gmail.com>
2023-04-23 23:00:53 +0200

    DTOS

---
+++ TimeCapsuleBackend.Data/DTOs/TimeCapsuleDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeCapsuleBackend.Data.Models;
+
+namespace TimeCapsuleBackend.Data.DTOs
+{
+    public class TimeCapsuleDTO
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime OpeningDate { get; set; }
+        public int UserId { get; set; }
+    }
+}
