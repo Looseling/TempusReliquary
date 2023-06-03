569bafdfd2bb4ae358572004b9b7bde4a262f073
Khan <batyrkhan.akzholov@gmail.com>
2023-06-03 20:56:45 +0200

    update DTO

--- Backend/TimeCapsuleBackend.Data/DTOs/UserDTO.cs
+++ Backend/TimeCapsuleBackend.Data/DTOs/UserDTO.cs
@@ -9,8 +9,8 @@ namespace TimeCapsuleBackend.Data.DTOs
     public class UserDTO
     {
         public int Id { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
         public string Password { get; set; }
     }
 }
