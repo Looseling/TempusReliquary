f7fe94a1ed99f86697032f1c106c77470a11e9e3
Khan <batyrkhan.akzholov@gmail.com>
2023-06-03 21:00:18 +0200

    add GetCurrentUser

---
+++ Backend/TimeCapsuleBackend/Helper/HelperFunctions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Security.Claims;
+using TimeCapsuleBackend.Data.Models;
+
+namespace TimeCapsuleBackend.Helper
+{
+    public class HelperFunctions
+    {
+        public static User GetCurrentUser(HttpContext httpContext)
+        {
+            var identity = httpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                return new User
+                {
+                    Username = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    Email = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Email)?.Value,
+                };
+            }
+
+            return null;
+        }
+    }
+}
