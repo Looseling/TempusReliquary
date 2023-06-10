64fa45d215e45f077a7d1dfbbe62c92bf7c3413c
Khan <batyrkhan.akzholov@gmail.com>
2023-06-10 18:05:59 +0200

    add SASToken to links

--- Backend/BussinessLogic/Services/BlobService.cs
+++ Backend/BussinessLogic/Services/BlobService.cs
@@ -8,39 +8,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Azure.Storage.Sas;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
 
 namespace BussinessLogic.Services
 {
     public class BlobService : IBlobService
     {
         private readonly BlobServiceClient _blobServiceClient;
+        private readonly IConfiguration _config;
 
-        public BlobService(BlobServiceClient blobServiceClient)
+
+        public BlobService(BlobServiceClient blobServiceClient, IConfiguration config)
         {
             _blobServiceClient = blobServiceClient;
-        } 
+            _config = config;
+        }
 
         public Task DeleteBlobAsync(string blobname)
         {
             throw new NotImplementedException();
         }
 
-
-        public async Task<IEnumerable<string>> ListBlobsAsync(string userName,string timeCapsulse)
+        public async Task<IEnumerable<string>> ListBlobsAsync(string userName, string timeCapsule)
         {
             var containerInstance = _blobServiceClient.GetBlobContainerClient("timecapsule");
             var blobs = new List<string>();
-            var folderPrefix = $"{userName}/{timeCapsulse}/";
+            var folderPrefix = $"{userName}/{timeCapsule}/";
 
             await foreach (var blobItem in containerInstance.GetBlobsByHierarchyAsync(prefix: folderPrefix, delimiter: "/"))
             {
                 if (!blobItem.IsPrefix)
                 {
-                    var blobUrl = containerInstance.GetBlobClient(blobItem.Blob.Name).Uri.ToString();
+                    // Construct the URL for each blob with the SAS token.
+                    string blobUrl = GetBlobSasUrl("timecapsule", blobItem.Blob.Name);
+
                     blobs.Add(blobUrl);
                 }
             }
-
             return blobs;
         }
 
@@ -68,6 +74,34 @@ namespace BussinessLogic.Services
                 memoryStream.Seek(0, SeekOrigin.Begin);
                 await blobInstance.UploadAsync(memoryStream);
             }
+        }
+
+        public string GenerateSasTokenForContainer(string accountName, string accountKey, string containerName, BlobContainerSasPermissions permissions, TimeSpan validityPeriod)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(_config.GetConnectionString("AzureBlobStorage"));
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            BlobSasBuilder sasBuilder = new BlobSasBuilder()
+            {
+                BlobContainerName = containerName,
+                StartsOn = DateTimeOffset.UtcNow,
+                ExpiresOn = DateTimeOffset.UtcNow.Add(validityPeriod)
+            };
+            sasBuilder.SetPermissions(permissions);  
+            StorageSharedKeyCredential sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey);
+            string sasToken = sasBuilder.ToSasQueryParameters(sharedKeyCredential).ToString();
+            return sasToken;
+        }
+
+        public string GetBlobSasUrl(string containerName,  string pathToFile)
+        {
+
+            var match = Regex.Match(_config.GetConnectionString("AzureBlobStorage"), "AccountName=(?<accountName>[^;]+);AccountKey=(?<accountKey>[^;]+)");
+            string accountName = match.Groups["accountName"].Value;
+            string accountKey = match.Groups["accountKey"].Value;
+
+            string sasToken = GenerateSasTokenForContainer(accountName, accountKey, containerName, BlobContainerSasPermissions.Read, TimeSpan.FromMinutes(5));
+
+            return $"https://{accountName}.blob.core.windows.net/{containerName}/{pathToFile}?{sasToken}";
         }
     }
 }
