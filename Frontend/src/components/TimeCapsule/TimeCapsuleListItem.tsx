20c1323250263f9ba1068cf3ca0e56f4fd13b942
Khan <batyrkhan.akzholov@gmail.com>
2023-08-05 17:42:53 +0200

    update cards

--- Frontend/src/components/TimeCapsule/TimeCapsuleListItem.tsx
+++ Frontend/src/components/TimeCapsule/TimeCapsuleListItem.tsx
@@ -1,24 +1,37 @@
 import { Button, Card } from "react-bootstrap";
 import ITimeCapsuleModel from "../../types/TimeCapsuleModel";
+import "../../css/globalStyles.css";
+import { Link } from "react-router-dom";
 
 type ListItemProps = {
   timeCapsule: ITimeCapsuleModel;
 };
 
 function TimeCapsuleListItem({ timeCapsule }: ListItemProps) {
+  const cardColor = timeCapsule.isUploaded
+    ? "uploaded-card"
+    : "not-uploaded-card";
+  const redirectTo = timeCapsule.isUploaded
+    ? `/ViewTimeCapsule/${timeCapsule.id}`
+    : `/TimeCapsule/detail/${timeCapsule.id}`;
+
+  const redirectButtonText = timeCapsule.isUploaded ? "View" : "Edit";
+  const redirectButtonColor = timeCapsule.isUploaded ? "primary" : "success";
+
+  const date = new Date(timeCapsule.openingDate);
+  const formattedDate = `${date.getDate()}/${
+    date.getMonth() + 1
+  }/${date.getFullYear()}`;
+
   return (
-    <Card className="h-100">
-      <Card.Img
-        variant="top"
-        src={"images/DefaultCapsule.jpg"}
-        height="200px"
-        style={{ objectFit: "cover" }}
-      ></Card.Img>
-      <Card.Body className="d-flex flex-column">
-        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
-          <span className="fs-2">{timeCapsule.title}</span>
-          <span className="ms-2 text-muted">{timeCapsule.openingDate}</span>
-        </Card.Title>
+    <Card style={{ width: "18rem" }} className="disabled">
+      <Card.Img variant="top" src={"images/DefaultCapsule.jpg"}></Card.Img>
+      <Card.Body className={`${cardColor}`}>
+        <Card.Title className={`${cardColor}`}>{timeCapsule.title}</Card.Title>
+        <Card.Text className={`${cardColor}`}>{formattedDate}</Card.Text>
+        <Link to={`${redirectTo}`}>
+          <Button variant={redirectButtonColor}>{redirectButtonText}</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
