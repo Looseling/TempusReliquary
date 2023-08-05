696241c4d2b503895ffcf04ccbe4aac8a243b2a7
Khan <batyrkhan.akzholov@gmail.com>
2023-08-05 17:45:44 +0200

    Add login logout no NAVBAR

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,22 +1,52 @@
 import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const history = useNavigate();
+
+  // HANDLE LOGOUT EVENT
+  const handleLogout = (e: any) => {
+    e.preventDefault();
+    console.log("Logout");
+
+    // CLEAR DATA FROM STORAGE
+    localStorage.clear();
+    sessionStorage.clear();
+
+    history("/Public");
+  };
+
   return (
-    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+    <NavbarBs sticky="top" className="bg-primary shadow-sm mb-3">
       <Container>
+        <NavbarBs.Brand as={NavLink} to="/">
+          TimeReliguary
+        </NavbarBs.Brand>
         <Nav className="me-auto">
-          <Nav.Link to="/timecapsule" as={NavLink}>
-            TimeCapsules
-          </Nav.Link>
-          <Nav.Link to="/register" as={NavLink}>
-            register
-          </Nav.Link>
-          <Nav.Link to="/login" as={NavLink}>
-            login
-          </Nav.Link>
+          <Nav.Link href="/TimeCapsule">Time Capsules</Nav.Link>
+          <Nav.Link href="/about">about</Nav.Link>
         </Nav>
       </Container>
+
+      <Nav className="me-auto justify-content-end">
+        {localStorage.getItem("UserName") == null && (
+          <>
+            <Nav.Link to="/register" as={NavLink}>
+              Register
+            </Nav.Link>
+            <Nav.Link to="/login" as={NavLink}>
+              Login
+            </Nav.Link>
+          </>
+        )}
+        {localStorage.getItem("UserName") != null && (
+          <>
+            <NavbarBs.Text>{localStorage.getItem("UserName")}</NavbarBs.Text>
+            <Button onClick={handleLogout}>logout</Button>
+          </>
+        )}
+      </Nav>
     </NavbarBs>
   );
 }
