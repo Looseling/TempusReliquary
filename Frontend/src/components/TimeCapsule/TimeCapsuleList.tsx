6d7eda78fe87722507e5406c8bbacc2a17d69d1d
Khan <batyrkhan.akzholov@gmail.com>
2023-08-05 17:42:40 +0200

    update rights for public/private TC

--- Frontend/src/components/TimeCapsule/TimeCapsuleList.tsx
+++ Frontend/src/components/TimeCapsule/TimeCapsuleList.tsx
@@ -4,12 +4,13 @@ import TimeCapsuleService from "../../services/TimeCapsuleService";
 import { Card, Col, Row, Table } from "react-bootstrap";
 import TimeCapsuleListItem from "./TimeCapsuleListItem";
 import { Link } from "react-router-dom";
+import PreviewTimeCapsule from "../../pages/ViewTimeCapsule";
 
 type TimeCapsuleListProps = {
-  TableChanged: boolean;
+  viewMode: string;
 };
 
-function TimeCapsuleList() {
+function TimeCapsuleList({ viewMode }: TimeCapsuleListProps) {
   const [TimeCapsules, SetTimeCapsules] = useState<ITimeCapsuleModel[]>();
   const [AddTimeCapsule, SetAddTimeCapsule] = useState<boolean>(false);
   useEffect(() => {
@@ -17,28 +18,41 @@ function TimeCapsuleList() {
   }, []);
 
   const retrieveTimeCapsule = () => {
-    TimeCapsuleService.getAll()
-      .then((response) => {
-        SetTimeCapsules(response.data);
-        console.log(response.data);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
+    if (viewMode == "user") {
+      TimeCapsuleService.getAll()
+        .then((response) => {
+          SetTimeCapsules(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    } else if (viewMode == "public") {
+      TimeCapsuleService.getAllUploaded()
+        .then((response) => {
+          SetTimeCapsules(response.data);
+          console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
   };
 
-  const refreshList = () => {
-    retrieveTimeCapsule();
-  };
+  // const refreshList = () => {
+  //   retrieveTimeCapsule();
+  // };
   return (
     <>
       <Row md={3} xs={1} lg={4} className="g-3">
         {TimeCapsules?.map((TimeCapsule) => (
           <Col key={TimeCapsule.id}>
-            <div onClick={() => console.log(1)}>
-              <Link to={`/TimeCapsule/detail/${TimeCapsule.id}`}>
+            <div>
+              {viewMode == "user" && (
                 <TimeCapsuleListItem timeCapsule={TimeCapsule} />
-              </Link>
+              )}
+              {viewMode == "public" && (
+                <TimeCapsuleListItem timeCapsule={TimeCapsule} />
+              )}
             </div>
           </Col>
         ))}
